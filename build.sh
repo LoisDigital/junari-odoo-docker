9775aae3b3d142fa79511916d95511f1309e4c74
Loïs Talagrand <91759903+LoisDigital@users.noreply.github.com>
2023-11-13 09:45:11 -1000

    [REM] Remove useless files

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source build.env
-
-echo "Building the junari/odoo:$ODOO_VERSION image..."
-docker build \
-    --build-arg ODOO_VERSION=$ODOO_VERSION \
-    --build-arg ODOO_REVISION=$ODOO_REVISION \
-    -t junari/odoo:$ODOO_VERSION .
--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-
-source build.env
-
-echo "Starting the junari/odoo:$ODOO_VERSION image for development ..."
-docker run --rm -it \
-    -v junari-odoo-data-$ODOO_VERSION:/opt/odoo/data \
-    -p 8069:8069 --env-file=odoo.env junari/odoo:$ODOO_VERSION $@
